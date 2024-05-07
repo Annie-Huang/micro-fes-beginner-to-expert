6df1f3c679d71f00512d4657f3eadfd03c1e0b76
Annie Huang <annie.hn@gmail.com>
2024-05-07 21:33:23 +1000

    Reverse back the demo change

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -2,18 +2,18 @@ import React, { Suspense, useState } from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.scss';
-// import Header from 'home/Header';
+import Header from 'home/Header';
 import Footer from 'home/Footer';
 
-/*const App = () => (
+const App = () => (
   <div className='text-3xl mx-auto max-w-6xl'>
     <Header />
     <div className='my-10'>Home Page Content</div>
     <Footer />
   </div>
-);*/
+);
 
-const Header = React.lazy(() => import('home/Header'));
+/*const Header = React.lazy(() => import('home/Header'));
 const App = () => {
   // Note04 - Before we click button to show the Header.jpg
   // Note05 - After we click button to show the Header.jpg
@@ -33,5 +33,5 @@ const App = () => {
       <Footer />
     </div>
   );
-};
+};*/
 ReactDOM.render(<App />, document.getElementById('app'));
