b41a1d73cad6d210d69910556c44486a131ea4cc
Annie Huang <annie.hn@gmail.com>
2024-05-09 21:56:41 +1000

    Change template to display the whole product information

--- pdp/src/PDPContent.jsx
+++ pdp/src/PDPContent.jsx
@@ -16,7 +16,23 @@ const PdpContent = () => {
   }, []);
 
   if (!product) return null;
-  return <div>Product {product.name}</div>;
+  return (
+    <div className='grid grid-cols-2 gap-5'>
+      <div>
+        <img src={product.image} alt={product.name} />
+      </div>
+      <div>
+        <div className='flex'>
+          <h1 className='font-bold text-3xl flex-grow'>{product.name}</h1>
+          <div className='font-bold text-3xl flex-end'>
+            {currency.format(product.price)}
+          </div>
+        </div>
+        <div className='mt-10'>{product.description}</div>
+        <div className='mt-10'>{product.longDescription}</div>
+      </div>
+    </div>
+  );
 };
 
 export default PdpContent;
