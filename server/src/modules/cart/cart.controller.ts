28e84804a12a5dec9e35e85b4ae88b117b3bb9c8
Annie Huang <annie.hn@gmail.com>
2024-05-10 21:22:11 +1000

    Add the interface definition of CartItem, Cart, and a helper function of initialCart in cart.controller.ts

--- server/src/modules/cart/cart.controller.ts
+++ server/src/modules/cart/cart.controller.ts
@@ -1,6 +1,23 @@
 import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../../auth/jwt-auth.guard';
 
+import products, { Product } from '../../products';
+
+interface CartItem extends Product {
+  quantity: number;
+}
+
+interface Cart {
+  cartItems: CartItem[];
+}
+
+const initialCart = (indexes: number[]): Cart => ({
+  cartItems: indexes.map((index) => ({
+    ...products[index],
+    quantity: 1,
+  })),
+});
+
 @Controller('cart')
 export class CartController {
   constructor() {}
