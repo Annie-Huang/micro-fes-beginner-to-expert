b1366c525976ec39871d3ef74320955dd66c7251
Annie Huang <annie.hn@gmail.com>
2024-05-11 16:04:01 +1000

    Create template that once we showCart by clicking on the shoppingCart icon after login, show a popper of all the items details of the cart. Tested and it worked

--- cart/src/MiniCart.jsx
+++ cart/src/MiniCart.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
+
 import { cart } from './cart';
+import { currency } from 'home/products';
 
 const MiniCart = () => {
   const [items, setItems] = useState(undefined);
@@ -22,6 +24,36 @@ const MiniCart = () => {
         <i className='ri-shopping-cart-2-fill text-2xl' id='showcart'></i>
         {items.length}
       </span>
+      {showCart && (
+        <>
+          <div
+            className='absolute p-5 border-4 border-blue-800 bg-white rounded-xl text-black'
+            style={{
+              width: 300,
+              top: '2rem',
+              // left: -250,
+            }}
+          >
+            <div
+              className='grid gap-3 text-sm'
+              style={{
+                gridTemplateColumns: '1fr 3fr 10fr 2fr',
+              }}
+            >
+              {items.map((item) => (
+                <React.Fragment key={item.id}>
+                  <div>{item.quantity}</div>
+                  <img src={item.image} alt={item.name} className='max-h-6' />
+                  <div>{item.name}</div>
+                  <div className='text-right'>
+                    {currency.format(item.quantity * item.price)}
+                  </div>
+                </React.Fragment>
+              ))}
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 };
