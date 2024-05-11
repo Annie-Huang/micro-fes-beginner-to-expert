dae95e2ea47c43484690a7226271b526229768c2
Annie Huang <annie.hn@gmail.com>
2024-05-11 17:56:25 +1000

    Add login in the <HomeContent> that once we login in <Header>, each product card will display a 'Add to Cart' button

--- home/src/HomeContent.jsx
+++ home/src/HomeContent.jsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from 'react';
+
 import { currency, getProducts } from './products';
+import { addToCart, useLoggedIn } from 'cart/cart';
 
 const HomeContent = () => {
+  const loggedIn = useLoggedIn();
   const [products, setProducts] = useState([]);
+
   useEffect(() => {
     getProducts().then(setProducts);
   }, []);
+
   return (
     <div className='grid grid-cols-4 gap-5'>
       {products.map((product) => (
@@ -18,6 +23,17 @@ const HomeContent = () => {
             <div className='flex-end'>{currency.format(product.price)}</div>
           </div>
           <div className='text-sm mt-4'>{product.description}</div>
+          {loggedIn && (
+            <div className='text-right mt-2'>
+              <button
+                className='bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-2 px-4 rounded'
+                onClick={() => addToCart(product.id)}
+                id={`addtocart_${product.id}`}
+              >
+                Add to Cart
+              </button>
+            </div>
+          )}
         </div>
       ))}
     </div>
