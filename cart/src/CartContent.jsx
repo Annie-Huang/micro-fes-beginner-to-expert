02dc42d60e1dacf0c63a21a17a9fb834fd90775c
Annie Huang <annie.hn@gmail.com>
2024-05-11 20:50:42 +1000

    Rewrite CartContent logic and template. Get cart items info and display them. Very similar to <MiniCart>

--- cart/src/CartContent.jsx
+++ cart/src/CartContent.jsx
@@ -1,10 +1,39 @@
 import React, { useEffect, useState } from 'react';
 
-import { login, jwt } from './cart';
-import Login from './Login';
-import MiniCart from './MiniCart';
+// import { login, jwt } from './cart';
+// import Login from './Login';
+// import MiniCart from './MiniCart';
+
+import { cart, clearCart } from 'cart/cart';
+import { currency } from 'home/products';
 
 const CartContent = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(
+    () => cart.subscribe((value) => setItems(value?.cartItems ?? [])),
+    []
+  );
+
+  return (
+    <>
+      <div className='my-10 grid grid-cols-4 gap-5'>
+        {items.map((item) => (
+          <React.Fragment key={item.id}>
+            <div>{item.quantity}</div>
+            <img src={item.image} alt={item.name} className='max-h-6' />
+            <div>{item.name}</div>
+            <div className='text-right'>
+              {currency.format(item.quantity * item.price)}
+            </div>
+          </React.Fragment>
+        ))}
+      </div>
+    </>
+  );
+};
+
+/*const CartContent = () => {
   const [token, setToken] = useState('');
 
   useEffect(() => {
@@ -22,6 +51,6 @@ const CartContent = () => {
       <MiniCart />
     </div>
   );
-};
+};*/
 
 export default CartContent;
