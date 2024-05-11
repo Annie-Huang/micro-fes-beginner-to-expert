f42d43c98e51ca972c4956d86a2110bc415a8443
Annie Huang <annie.hn@gmail.com>
2024-05-11 10:13:00 +1000

    Make call in login and make sure it's successfully log in devtool

--- cart/src/CartContent.jsx
+++ cart/src/CartContent.jsx
@@ -1,9 +1,18 @@
 import React, { useEffect, useState } from 'react';
 
+import { login, jwt } from './cart';
+
 const CartContent = () => {
   const [token, setToken] = useState('');
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    // Can verify the call is successfully made in the network tab in devtool
+    login('sally', '123');
+
+    // The subscribe function for BehaviorSubject will return a unsubscribe, so perfect to hook it into return in useEffect
+    return jwt.subscribe((val) => setToken(val ?? ''));
+  }, []);
+
   return <div>JWT: {token}</div>;
 };
 
