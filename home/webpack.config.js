22d400b522c4d9396cfccfcc22930f273fba27d5
Annie Huang <annie.hn@gmail.com>
2024-05-12 11:14:27 +1000

    Expose CartContent, HomeContent and PDPContent in their own webpack.config

--- home/webpack.config.js
+++ home/webpack.config.js
@@ -61,6 +61,7 @@ module.exports = (_, argv) => ({
         './Header': './src/Header.jsx',
         './Footer': './src/Footer.jsx',
         './products': './src/products.js',
+        './HomeContent': './src/HomeContent.jsx',
       },
       shared: {
         ...deps,
