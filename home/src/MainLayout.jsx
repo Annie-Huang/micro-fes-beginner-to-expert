558a8d1ea2b01f2b02fc89867c886c3851911ff1
Annie Huang <annie.hn@gmail.com>
2024-05-12 11:23:32 +1000

    Create <MainLayout> in Home app. It's a copy of <App> in PDP app with adjustment because PDP got react-router-dom.

---
+++ home/src/MainLayout.jsx
@@ -0,0 +1,28 @@
+import React, { Suspense, useState } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import 'remixicon/fonts/remixicon.css';
+import './index.scss';
+
+import Header from 'home/Header';
+import Footer from 'home/Footer';
+import PDPContent from 'pdp/PDPContent';
+import HomeContent from 'home/HomeContent';
+import CartContent from 'cart/CartContent';
+
+const MainLayout = () => (
+  <BrowserRouter>
+    <div className='text-3xl mx-auto max-w-6xl'>
+      <Header />
+      <div className='my-10'>
+        <Routes>
+          <Route path='/product/:id' element={<PDPContent />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  </BrowserRouter>
+);
+
+export default MainLayout;
